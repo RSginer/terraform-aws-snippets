029511069175ffa0cf03dda596fcfff3ad93a342
Rubén Soler <ruben.soler@exp360.com>
2019-03-11 13:17:07 +0100

    Feature provider in separate file

--- instances.tf
+++ instances.tf
@@ -1,9 +1,9 @@
-# Configure the AWS Provider
-provider "aws" {
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
-  region  = "eu-west-1"
-}
+# Configure the AWS Provider, copy this code into a provider.tf file
+# provider "aws" {
+#   access_key = "YOUR_ACCESS_KEY"
+#   secret_key = "YOUR_SECRET_KEY"
+#   region  = "eu-west-1"
+# }
 
 resource "aws_instance" "firstdemo" {
   ami  = "ami-922914f7"
