20ed77e9d2c692f1e5609306693a827cc8108c38
Rubén Soler <ruben.soler@exp360.com>
2019-03-12 16:54:16 +0100

    Feature deploy different ami depends of region

--- variables.tf
+++ variables.tf
@@ -1,9 +1,15 @@
-variable "amitype" {
-  default = "ami-08d658f84a6d84a80" // Ubuntu Server 18.04 LTS (HVM) SSD 64-bit x86
+variable "region" {
+  default = "eu-west-1"
 }
 
-variable "env" {
+variable "amitype" {
+  type = "map"
+  default = {
+    eu-west-1 = "ami-08d658f84a6d84a80" 
+  } // Ubuntu Server 18.04 LTS (HVM) SSD 64-bit x86
 }
+
+variable "env" { }
 
 variable "instancesName" {
   type = "map"
