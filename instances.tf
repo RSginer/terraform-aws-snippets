487db01cf6f86ef95b07e0e064f91fc02189467c
Rubén Soler <ruben.soler@exp360.com>
2019-03-20 10:33:47 +0100

    Feature data sources

--- instances.tf
+++ instances.tf
@@ -5,10 +5,16 @@
 #   region  = "eu-west-1"
 # }
 
+data "aws_availability_zones" "available" {
+  
+}
+
+
 resource "aws_instance" "firstdemo" {
   count = 3
   ami  = "${lookup(var.amitype, var.region)}"
   instance_type = "t2.micro"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
 
   tags {
     Name = "${lookup(var.instancesName, var.env)}-${count.index}" 
